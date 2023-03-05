739813b4846a49c23262199d4b78f425eef4ffcc
Utkarsh <utkarshmishra2001@gmail.com>
2023-03-05 17:08:15 +0530

    Completed pomodoro page

--- src/screens/Main/Pomodoro/Pomodoro.tsx
+++ src/screens/Main/Pomodoro/Pomodoro.tsx
@@ -1,11 +1,54 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 import styles from './styles';
+import colors from '../../../constants/colors';
+import {AnimatedCircularProgress} from 'react-native-circular-progress';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Pomodoro = () => {
   return (
-    <View>
-      <Text>Pomodoro</Text>
+    <View style={styles.pageContainer}>
+      <View style={styles.taskContainer}>
+        <View style={styles.taskLeft}>
+          <Text style={styles.taskTitle}>Task Title</Text>
+          <Text style={styles.taskTime}>120 minutes</Text>
+        </View>
+        <View style={styles.taskRight}>
+          <Text style={styles.sessionStatus}>2/4</Text>
+          <Text style={styles.sessionTime}>25 minutes</Text>
+        </View>
+      </View>
+      <View style={styles.counterContainer}>
+        <AnimatedCircularProgress
+          size={300}
+          width={25}
+          fill={65}
+          tintColor={colors.theme_red}
+          onAnimationComplete={() => console.log('onAnimationComplete')}
+          backgroundColor={colors.theme_gray}
+          fillLineCap={'round'}
+          rotation={0}
+        />
+        <View style={styles.pomodoroDetails}>
+          <Text style={styles.pomodoroTimeLeft}>18:59</Text>
+          <Text style={styles.pomodoroLeft}>2 of 4 sessions</Text>
+        </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Text style={styles.currentTask}>Stay focus for 25 minutes</Text>
+        <View style={styles.controlButtons}>
+          <TouchableOpacity style={styles.resetBtn}>
+            <FontAwesome5 name="redo" size={20} color={colors.theme_gray} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.pauseBtn}>
+            <Ionicons name="pause" size={40} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.cancel}>
+            <FontAwesome5 name="stop" size={20} color={colors.theme_gray} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/Main/Pomodoro/styles.ts
+++ src/screens/Main/Pomodoro/styles.ts
@@ -1,7 +1,124 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../../constants/colors';
 
 const styles = StyleSheet.create({
-
-})
+    pageContainer: {
+        flex: 1,
+        paddingHorizontal: 20,
+        backgroundColor: 'black'
+    },
+    taskContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginVertical: 30,
+        backgroundColor: colors.theme_dark,
+        borderRadius: 10,
+        padding: 10
+    },
+    taskLeft: {
+        columnGap: 10,
+        padding: 10,
+    },
+    taskTitle: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    taskTime: {
+        color: colors.theme_gray,
+        fontSize: 14,
+        marginVertical: 5,
+        fontWeight: 'bold'
+    },
+    taskRight: {
+        padding: 20,
+        columnGap: 10,
+        alignItems: 'flex-end',
+    },
+    sessionStatus: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    sessionTime: {
+        color: colors.theme_gray,
+        fontSize: 14,
+        marginVertical: 5,
+        fontWeight: 'bold'
+    },
+    counterContainer: {
+        position: 'relative',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 10,
+    },
+    pomodoroDetails: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    pomodoroTimeLeft: {
+        color: 'white',
+        fontSize: 40,
+        fontWeight: 'bold'
+    },
+    pomodoroLeft: {
+        color: colors.theme_gray,
+        fontSize: 14,
+        marginVertical: 5,
+        fontWeight: 'bold'
+    },
+    buttonContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 10,
+        rowGap: 10,
+    },
+    currentTask: {
+        color: 'white',
+        fontSize: 14,
+        marginVertical: 5,
+        fontWeight: 'bold'
+    },
+    controlButtons: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        width: '100%',
+        padding: 10,
+    },
+    resetBtn: {
+        backgroundColor: colors.theme_dark,
+        borderRadius: 50,
+        padding: 10,
+        width: 50,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    pauseBtn: {
+        backgroundColor: colors.theme_dark,
+        borderRadius: 50,
+        padding: 10,
+        width: 90,
+        height: 90,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    cancel: {
+        backgroundColor: colors.theme_dark,
+        borderRadius: 50,
+        padding: 10,
+        width: 50,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
 
 export default styles;
