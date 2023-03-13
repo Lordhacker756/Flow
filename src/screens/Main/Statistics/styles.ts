93957dc0146e0c36507c69db6c19e2fc58858855
Utkarsh <utkarshmishra2001@gmail.com>
2023-03-13 19:33:42 +0530

    bebugged selected box going behind list box

--- src/screens/Main/Statistics/styles.ts
+++ src/screens/Main/Statistics/styles.ts
@@ -63,6 +63,7 @@ const styles = StyleSheet.create({
     allTaskContainer: {
         paddingHorizontal: responsiveWidth(6),
         marginTop: responsiveHeight(2),
+        zIndex: -1,
     },
     allTaskHeader: {
         flexDirection: 'row',
